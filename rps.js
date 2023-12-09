2fa25272ab2ff0edf6379ce30fce6e8cb9fe1220
Lucas Chaves Meyles <lchavesmeyles@gmail.com>
2023-12-09 10:31:55 -0800

    Added UI to the Rock Paper Scissors game

--- rps.js
+++ rps.js
@@ -1,11 +1,11 @@
 
 //0 is rock, 1 is paper, 2 is scissors
-rps = {
+const rps = {
     'rock': 0,
     'paper': 1,
     'scissors': 2
 };
-rpsReverse = {
+const rpsReverse = {
     0: 'Rock',
     1: 'Paper',
     2: 'Scissors'
@@ -18,38 +18,37 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     //only accepts 0, 1, or 2
+    let announce = document.querySelector('#announcement');
+
+
     if (playerSelection == computerSelection) {
-        console.log("It's a tie! Try again.");
-        return 0;
+        announce.textContent = 'It\'s a tie! Try again.';
     }
     else if (computerSelection - playerSelection == 1 || computerSelection - playerSelection == -2) {
-        console.log(`You Lose! ${rpsReverse[computerSelection]} beats ${rpsReverse[playerSelection]}`);
-        return -1;
+        announce.textContent = `You Lose! ${rpsReverse[computerSelection]} beats ${rpsReverse[playerSelection]}`;
+        cScore.textContent = Number(cScore.textContent) + 1;
     }
     else if (computerSelection - playerSelection == -1 || computerSelection - playerSelection == 2) {
-        console.log(`You Win! ${rpsReverse[playerSelection]} beats ${rpsReverse[computerSelection]}`);
-        return 1;
+        announce.textContent = `You Win! ${rpsReverse[playerSelection]} beats ${rpsReverse[computerSelection]}`;
+        pScore.textContent = Number(pScore.textContent) + 1;
+    }
+
+    if (Number(pScore.textContent) === 5) {
+        announce.textContent = 'Congrats, you won!';
+    }
+    if (Number(cScore.textContent) === 5) {
+        announce.textContent = 'Oh no, you lost!';
     }
 }
 
-function game () {
-    pScore = 0;
-    cScore = 0;
-    while (pScore <= 2 && cScore <= 2) {
-        let pSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        let score = playRound(rps[pSelection], getComputerChoice());
-        if (score == 1) {
-            pScore += 1;
-        }
-        else if (score == -1) {
-            cScore += 1;
-        }
-    }
-    if (pScore > 2) {
-        console.log("Congrats, you won!");
-    }
-    else if (cScore > 2) {
-        console.log("Oh no, you lost!");
-    }
-}
-console.log(game());
+    const rockButton = document.querySelector('#rock');
+    rockButton.addEventListener('click', () => playRound(0, getComputerChoice()));
+
+    const paperButton = document.querySelector('#paper');
+    paperButton.addEventListener('click', () => playRound(1, getComputerChoice()));
+
+    const scissorButton = document.querySelector('#scissors');
+    scissorButton.addEventListener('click', () => playRound(2, getComputerChoice()));
+
+    const pScore = document.querySelector('#pScore');
+    const cScore = document.querySelector('#cScore');
